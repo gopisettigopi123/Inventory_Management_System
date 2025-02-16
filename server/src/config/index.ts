9e0376c31aa031a9e9e1af14a6e6e57274805ecf
GOPISETTI GOPI <95520388+gopisettigopi123@users.noreply.github.com>
2025-02-16 08:56:05 +0530

    index.ts

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -5,7 +5,7 @@ dotenv.config({ path: path.join((process.cwd(), '.env')) });
 
 export default {
   nodeEnv: process.env.NODE_ENV,
-  port: process.env.PORT,
+  port: process.env.PORT || 4000,
   database_url: process.env.DATABASE_URL,
   jwt_secret: process.env.JWT_SECRET
 };
